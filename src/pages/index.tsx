75234a13751ee2b76b69a655770658d787b5bc0b
Casey Siebel <casey@Caseys-MacBook-Pro.local>
2022-01-31 22:29:18 -0800

    working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { trpc } from "@/utils/trpc";
 
 export default function Home() {
-  const { data, isLoading } = trpc.useQuery(["hello"], {});
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Casey" }]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (data) return <div>{data.greeting}</div>;
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder</div>
