078c285da007ef158000e5eb60d7e0b9d2889ada
Casey Siebel <casey@Caseys-MacBook-Pro.local>
2022-01-31 22:30:10 -0800

    self closing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       <div className="text-2xl text-center">Which Pokemon is Rounder</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-200" />
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-200" />
         <br />
       </div>
     </div>
